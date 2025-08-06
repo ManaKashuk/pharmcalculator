22cc9eea7a2aa9cb4300a0de9e18ec9500f98125
Mana Kashuk <140280356+ManaKashuk@users.noreply.github.com>
2025-08-06 15:51:45 -0500

    Update Pharmcalculator.py.app.py

--- Pharmcalculator.py.app.py
+++ Pharmcalculator.py.app.py
@@ -9,7 +9,7 @@ logo = Image.open("logo.jpg")
 st.image(logo, width=200)
 st.title("Dosage Calculators")
 st.markdown(
-    "<p style='font-size:28px; font-style:italic; color:#34495E;'>"
+    "<p style='font-size:26px; font-style:italic; color:#34495E;'>"
     "A free dosage calculator for pharmacists, undergraduate and graduate students in pharmaceutics and biomedical research."
     "</p>",
     unsafe_allow_html=True
